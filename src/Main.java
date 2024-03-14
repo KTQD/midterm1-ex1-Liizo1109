38376b53ba1824107d5bc1567711f8c54391ef99
Wynly._ <124950304+Liizo1109@users.noreply.github.com>
2024-03-14 08:04:20 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
         MP3 mp3 = new MP3("MP3 Player", "Portable music player", "MP3-001", 50.99f, 180);
         System.out.println("MP3 Info:");
         mp3.showInfo();
         System.out.println();
-
-        Book book = new Book("Java Programming", "Learn Java programming laguage", "BK-001", 29.99f, "John Doe", 400, "Programming");
-       System.out.println("Book Info:");
+        
+        Book book = new Book("Java Programming", "Learn Java programming language", "BK-001", 29.99f, "John Doe", 400, "Programming");
+        System.out.println("Book Info:");
         book.showInfo();
     }
 }
